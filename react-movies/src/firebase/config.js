9fe879b70824bc88150581c377b6c06ac44decfb
ayoog <20098135@mail.wit.ie>
2024-12-05 15:00:31 +0000

    reconigured .env file

--- react-movies/src/firebase/config.js
+++ react-movies/src/firebase/config.js
@@ -5,14 +5,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyx1cfVMN9TH8PP1uR1V2TR0wKW_89-sY",
-  authDomain: "movies-app-59866.firebaseapp.com",
-  databaseURL: "https://movies-app-59866-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "movies-app-59866",
-  storageBucket: "movies-app-59866.firebasestorage.app",
-  messagingSenderId: "149202355759",
-  appId: "1:149202355759:web:e1b005bf8e36eb287468da",
-  measurementId: "G-1PCVV812RJ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 
